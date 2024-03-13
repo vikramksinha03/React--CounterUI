72601c6d4ef104a70c94da3e30d625e9ab2abc7a
Vikram Sinha <micky@Vikrams-MacBook-Pro.local>
2024-03-13 22:23:17 +0530

    comitting entire code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(1)
+  const [count, setCount] = useState(1)
+
+  function handleDecrement() {
+    setNum(num - 1)
+  }
+
+  function handleIncrement() {
+    setNum(num + 1)
+  }
+
+  function handleCountDecrement() {
+    setCount(count - num)
+  }
+
+  function handleCountIncrement() {
+    setCount(count + num)
+  }
+
+  const date = new Date()
+  date.setDate(date.getDate() + count)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <div>
+        <button onClick={handleDecrement}>-</button> 
+        <span>Step: {num}</span>
+        <button onClick={handleIncrement}>+</button>
+     </div>
+     <div>
+        <button onClick={handleCountDecrement}>-</button> 
+        <span>Count: {count}</span>
+        <button onClick={handleCountIncrement}>+</button>
+     </div>
     </div>
+
+    <p>
+      {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+      <span>{date.toDateString()}</span>
+    </p>
+
+    </>
   );
 }
 
